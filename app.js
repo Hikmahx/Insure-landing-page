58c017e6aa6e0bcf92124b512d8710e293469979
Hikmahx <hikmayousuph@gmail.com>
2020-12-16 18:29:41 -0800

    nav UI var added

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 // UI variables
 const hamburger = document.querySelector('.hamburger-menu');
 const menuItems = document.querySelectorAll('.menu-item');
+const nav = document.querySelector('.nav-left')
 const menu = document.querySelector('.menu');
 const icon = document.querySelector('.menu-icon');
 const close = document.querySelector('.close');
 
 // Menu toggle event
 hamburger.addEventListener('click', (e)=>{
-  console.log('working');
+  nav.classList.toggle('change')
   menu.classList.toggle('change');
   icon.classList.toggle('change');
   close.classList.toggle('change')
